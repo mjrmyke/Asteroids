81d652e7e12f396bc0ec4ee6b2840ed0297343f2
mjrmyke <mjrmyke@users.noreply.github.com>
2015-04-09 12:19:26 -0700

    Removed Depreciated Code
    Deleted leftover fragments of broken code.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -25,8 +25,6 @@ MainWindow::MainWindow():QMainWindow()
 
     // connect message signal from scene to showMessage slot
 
-    /* add status bar message
-    statusBar()->showMessage("QSimulate has started");*/
 
   // create scene and central widget view of scene
   scene = new Scene();
@@ -37,20 +35,7 @@ MainWindow::MainWindow():QMainWindow()
   view->setBackgroundBrush(QBrush(QImage(":/images/bg.png")));
   setCentralWidget(view);
 
-  /*// create an item to add to the scene
-  mainship *ship = new mainship();
-  ship->setTransformOriginPoint(12.5,12.5);
-  ship->setRect(0,0,25,25); // change the rect size
-  ship->setPen(QPen(Qt::red, 5));
-  QPainter::SmoothPixmapTransform;
-  ship->setPos(400,300);*/
-
-  //add the item to the scene
-  //scene->addItem(ship);
-
-  /*// make rect focusable
-  ship->setFlag(QGraphicsItem::ItemIsFocusable);
-  ship->setFocus();*/
+  
 }
 
 void MainWindow::StartButton_Clicked()
@@ -70,21 +55,3 @@ void MainWindow::StartButton_Clicked()
 
 
 
-/*
-void MainWindow:: newGame(Scene *scene)
-{
-
-    // create an item to add to the scene
-    mainship *ship = new mainship();
-    ship->setTransformOriginPoint(12.5,12.5);
-    ship->setRect(0,0,25,25); // change the rect size
-    ship->setPen(QPen(Qt::red, 5));
-    QPainter::SmoothPixmapTransform;
-    ship->setPos(width()/2,height()/2);
-
-    ship->setFlag(QGraphicsItem::ItemIsFocusable);
-    ship->setFocus();
-
-    scene->addItem(ship);
-}
-*/
